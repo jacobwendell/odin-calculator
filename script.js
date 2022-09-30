1f791ef65a45dbe56796d4586e8e51d941b17436
jacobwendell <jwendell2020@gmail.com>
2022-09-30 11:24:06 -0400

    Created Operators
    Add()
    Subtract()
    Multiply()
    Divide()

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Operations
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+function divide(num1, num2) {
+    return Number(num1) / Number(num2);
+}
