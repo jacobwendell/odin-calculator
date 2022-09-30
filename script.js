92a29a9de8345bf561de2efd50cc6f1cd190f633
jacobwendell <jwendell2020@gmail.com>
2022-09-30 11:27:59 -0400

    Created Operate Function
    Takes in an operator and num1 and num2
    Then performs the operation with the two numbers

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return Number(num1) / Number(num2);
 }
+
+// Operate Function 
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+}
