d1d178c841d4beb40efb5dceebb15a1e82f626ea
jacobwendell <jwendell2020@gmail.com>
2022-10-02 12:20:24 -0400

    Basic Computing functions added

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 // Use variables to hold and display items
 // X + y = Z , X becomes Z and then inputted value equals Y then compute again
+
+
+// Operations
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+function divide(num1, num2) {
+    return Number(num1) / Number(num2);
+}
+
+function modulo(num1, num2) {
+    return Number(num1) % Number(num2);
+}
+
+// Operate Function 
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    } else if (operator === "%") {
+        return modulo(num1, num2);
+    }
+}
