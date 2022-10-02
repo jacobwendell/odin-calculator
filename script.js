7ea8ac087b24d3c6fd0f25319c86ba5fad9db8ca
jacobwendell <jwendell2020@gmail.com>
2022-10-02 13:24:48 -0400

    Working Addbutton
    Need to refactor for other buttons, but code is much cleaner
    Code also has more promise and a goal

--- script.js
+++ script.js
@@ -3,19 +3,54 @@
 // MainDisplayValue becomes value1 after operator pressed
 let value1;
 let value2;
+let finalValue;
 let displayValue = "";
 let mainDisplayValue = "";
+let operator = "+";
+
 
 // Operation Buttons and functions
+const moduloButton = document.querySelector("#modulo");
+const divisionButton = document.querySelector("#divide");
+const addButton = document.querySelector("#add");
+const subractButton = document.querySelector("#subtract");
+const multiplyButton = document.querySelector("#multiply");
+const equalButton = document.querySelector("#equals");
 
+addButton.addEventListener("click", function() {
+    if (displayValue === "") {
+        console.log("Do nothing, need number input first");
+    } else if (displayValue.includes("+") === false) {
+        value1 = Number(mainDisplayValue);
+        mainDisplayValue = "";
+        displayValue += " + ";
+        operator = "+";
+        showBothDisplays();
+    }
+})
+
+equalButton.addEventListener("click", function() {
+    if (mainDisplayValue === finalValue) {
+        console.log("");
+    } else if (displayValue.includes(operator) === true) {
+        value2 = Number(mainDisplayValue);
+        finalValue = operate('+', value1, value2);
+        mainDisplayValue = finalValue;
+        showBothDisplays();
+    } 
+})
 
 // Function for number buttons
 const numButtons = document.querySelectorAll(".num-button");
 numButtons.forEach(element => {
     element.addEventListener("click", function() {
-        displayValue  += element.id;
-        mainDisplayValue += element.id;
-        showBothDisplays();
+        if (mainDisplayValue === finalValue){
+            console.log("")
+        } else {
+            displayValue  += element.id;
+            mainDisplayValue += element.id;
+            showBothDisplays();
+        }
     })
 })
 
